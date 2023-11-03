33a55a7ff070d3db47c0adb8a09583f6a7e9e8ea
JudisDev <judisson429@hotmail.com>
2023-11-02 21:00:06 -0300

    Feat(Ebook) - Adicionando a página

---
+++ src/Components/ebook.jsx
@@ -0,0 +1,14 @@
+const Ebook = () => {
+  return (
+    <div className="visualizacao-ebook">
+      <img src="https://via.placeholder.com/300x300.png" />
+      <button>+ Visualizar</button>
+      <div className="info">
+        <span>7 Passos para construir uma Mentalidade de Sucesso</span>
+        <span>$0.00</span>
+      </div>
+    </div>
+  );
+};
+
+export default Ebook;
