8b946dcd127b3a8368c581798059866eec78e59d
JudisDev <judisson429@hotmail.com>
2023-11-02 21:06:27 -0300

    Feat(Mais) - Criando Página

--- src/Pages/Mais.jsx
+++ src/Pages/Mais.jsx
@@ -1,5 +1,48 @@
 const Mais = () => {
-    return <h1>Mais</h1>;
-  };
-  
-  export default Mais;
+  return (
+    <div className="container-principal-mais">
+      <h1>Mais</h1>
+      <div className="perguntas">
+        <div className="pergunta-resposta">
+          <div className="pergunta">Para quem é indicado esse curso?</div>
+          <div className="resposta">
+            Nosso curso "Construindo uma Mentalidade de Sucesso" é direcionado
+            para todas as pessoas que desejam alcançar seus objetivos, superar
+            desafios e desenvolver uma mentalidade positiva e focada. Se você
+            está em busca de crescimento pessoal e profissional, busca melhorar
+            sua autoconfiança, enfrentar obstáculos com resiliência e cultivar
+            uma mentalidade de sucesso, este curso é para você!
+          </div>
+        </div>
+        <div className="pergunta-resposta">
+          <div className="pergunta">
+            Esse curso é adequado para profissionais de todas as áreas?
+          </div>
+          <div className="resposta">
+            O curso é projetado para profissionais de todas as áreas.
+            Independentemente de sua carreira ou background, as estratégias e
+            técnicas ensinadas são aplicáveis e trarão resultados benéficos
+            para qualquer pessoa que deseje crescer e prosperar em suas vidas
+            pessoais e profissionais.
+          </div>
+        </div>
+        <div className="pergunta-resposta">
+          <div className="pergunta">
+            O curso oferece algum tipo de suporte após sua conclusão?
+          </div>
+          <div className="resposta">
+            Sim! Valorizamos a jornada contínua de crescimento de nossos alunos.
+            Após a conclusão do curso, você terá acesso a suporte, incluindo
+            recursos adicionais, artigos exclusivos e até mesmo sessões de
+            acompanhamento com nossos coaches. Nossa equipe estará pronta para
+            apoiá-lo(a) em sua caminhada para o sucesso, fornecendo orientações
+            e motivação para ajudá-lo(a) a alcançar seus objetivos de longo
+            prazo.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Mais;
