7176dcc90636fed2f863d93d507d2ba02c12a998
JudisDev <judisson429@hotmail.com>
2023-11-02 21:02:04 -0300

    Feat(Treinamento) - Criando componente

---
+++ src/Components/Treinamento.jsx
@@ -0,0 +1,47 @@
+
+const Treinamento = () => {
+  return (
+    <div className="container-treinamento">
+    <h2>TREINAMENTO</h2>
+
+    <div className="conteudo">
+      <div className="elemento">
+        <img src="https://via.placeholder.com/300x300.png" />
+        <h4>Construindo Mentes de Sucesso</h4>
+        <span>
+          Um treinamento de desenvolvimento pessoal, para você libertar
+          todo o seu potencial e construir uma vida verdadeiramente
+          extraordinária, com exercícios práticos e técnicas comprovadas
+          cientificamente para que você consiga acessar todo seu poder
+          interior.
+        </span>
+      </div>
+      <div className="elemento">
+        <img src="https://via.placeholder.com/300x300.png" />
+        <h4>Vencendo a Procrastinação</h4>
+        <span>
+          Conquiste o sucesso! Supere a procrastinação e alcance suas
+          metas com nosso treinamento intensivo! Aprenda estratégias
+          comprovadas, domine o gerenciamento do tempo e vença o medo do
+          fracasso. Liberte seu verdadeiro potencial e transforme sua vida
+          pessoal e profissional.
+        </span>
+      </div>
+      <div className="elemento">
+        <img src="https://via.placeholder.com/300x300.png" />
+        <h4>Produtividade</h4>
+        <span>
+          Descubra o poder da produtividade exponencial! Aumente sua
+          eficiência, conquiste mais com menos esforço e alcance
+          resultados surpreendentes com nosso treinamento exclusivo. Não
+          perca essa oportunidade de se destacar e transformar sua rotina
+          profissional e pessoal. Eleve a sua produtividade a um novo
+          patamar.
+        </span>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default Treinamento;
