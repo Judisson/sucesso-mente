a0b8e354cdb768d2243470fd214597186b48810f
JudisP <judisson429@hotmail.com>
2023-11-03 17:35:59 -0300

    Style(Ebook) - Estilização do componente

--- src/Components/ebook.jsx
+++ src/Components/ebook.jsx
@@ -1,7 +1,7 @@
 const Ebook = () => {
   return (
     <div className="visualizacao-ebook">
-      <img src="https://via.placeholder.com/300x300.png" />
+      <img src="https://via.placeholder.com/280x380.png" />
       <button>+ Visualizar</button>
       <div className="info">
         <span>7 Passos para construir uma Mentalidade de Sucesso</span>
