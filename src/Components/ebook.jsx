9e7ce7138f8d9dde3fb2d7f80f146c8fe97c4212
JudisDev <judisson429@hotmail.com>
2023-11-08 11:04:12 -0300

    Feat(Ebook) - Usando o Modal

--- src/Components/ebook.jsx
+++ src/Components/ebook.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+import ModalEbook from "./Modal";
+
 const Ebook = () => {
+  const [showModalEbook, setShowModalEbook] = useState(false);
+
+  const toggle = () => {
+    setShowModalEbook((prev) => !prev);
+  };
+
   return (
     <div className="visualizacao-ebook">
       <img src="https://via.placeholder.com/280x380.png" />
-      <button>+ Visualizar</button>
+      <button className="button-modal" onClick={() => setShowModalEbook(true)}>+ Visualizar</button>
       <div className="info">
         <span>7 Passos para construir uma Mentalidade de Sucesso</span>
         <span>$0.00</span>
       </div>
+      <ModalEbook showModalEbook={showModalEbook} setShowModalEbook={setShowModalEbook} toggle={toggle} />
+
     </div>
   );
 };
 
 export default Ebook;
+
