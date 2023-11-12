431d9be2ff9f0991fa1164a4af1a593833036311
JudisDev <judisson429@hotmail.com>
2023-11-12 01:45:39 -0300

    Feat(FormEmail) - Criando o Component

---
+++ src/Components/FormEmail.jsx
@@ -0,0 +1,9 @@
+const FormEmail = () => {
+  return (
+            <div className="enviar-email">
+              <button type="submit">Enviar</button>
+            </div>
+  );
+};
+
+export default FormEmail;
