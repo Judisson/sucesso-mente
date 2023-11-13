d3cc488725eb6346863506b141c33f30257c44a0
JudisDev <judisson429@hotmail.com>
2023-11-13 09:52:36 -0300

    Feat - Formulário e Envio das informações na Api

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
 import Router from "./Router";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
 
 import "bootstrap/dist/css/bootstrap.css";
 import "@/scss/app.scss";
@@ -7,14 +8,18 @@ import "@/scss/app.scss";
 import TopBar from "@/Layout/Topbar/Topbar";
 import Footer from "@/Layout/Footer/Footer";
 
+import store from './store';
+
 function App() {
   return (
+    <Provider store={store}>
     <BrowserRouter>
       <TopBar />
 
       <Router />
       <Footer />
     </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/Components/FormEmail.jsx
+++ src/Components/FormEmail.jsx
@@ -1,8 +1,36 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { postEmail } from "@/redux/actions/emailActions";
+
+import { Field, Form } from "react-final-form";
+
 const FormEmail = () => {
+  const dispatch = useDispatch();
+
+  const onSubmit = (data) => {
+    console.log("data",data)
+
+    dispatch(postEmail(data.email));
+  };
+
   return (
+    <Form onSubmit={onSubmit} className="container-enviar-email">
+      {({ handleSubmit, form: { reset, pristine, submitting } }) => {
+        return (
+          <form onSubmit={handleSubmit} className="form-enviar-email">
             <div className="enviar-email">
+              <Field
+                type="email"
+                name="email"
+                placeholder="Email"
+                component="input"
+              ></Field>
               <button type="submit">Enviar</button>
             </div>
+          </form>
+        );
+      }}
+    </Form>
   );
 };
 
